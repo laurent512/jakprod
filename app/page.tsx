f5499ebad43d1bda2db8982a1482fbe8010de928
Laurent Tobaly <laurent.tobaly@gmail.com>
2023-11-13 15:39:57 +0100

    test

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,28 @@
 "use client";
 import Image from "next/image";
+import Scrollbars from "react-custom-scrollbars";
 import { Parallax, ParallaxBanner, ParallaxBannerLayer } from "react-scroll-parallax";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col">
+    <Scrollbars
+      renderThumbVertical={(props) => <div {...props} className="bg-gray-200 opacity-70 rounded-lg " />}
+      renderTrackVertical={(props) => <div {...props} className="h-full m-2  text-right justify-end right-0" />}
+      className="flex flex-col  min-h-screen  z-20 w-4"
+      // autoHeightMin={0}
+      // autoHeightMax={200}
+      // thumbMinSize={30}
+      autoHide
+      autoHideTimeout={400}
+      autoHideDuration={350}
+      universal>
       <Header2 />
       <div className=" h-screen w-full "> </div>
       <div className=" h-screen bg-green-400 w-full ">section 1 </div>
       <div className=" h-screen bg-red-400 w-full ">section 2 </div>
       <div className=" h-screen bg-blue-400 w-full ">section 3 </div>
       {/* <Component /> */}
-    </main>
+    </Scrollbars>
   );
 }
 
@@ -19,7 +30,7 @@ function Header2() {
   return (
     <Parallax className="w-full min-h-screen fixed top-0 left-0 -z-10">
       <video
-        className=" h-full w-full object-cover"
+        className=" h-screen w-full object-cover"
         autoPlay
         loop
         playsInline
--- app/parallaxprovider.tsx
+++ app/parallaxprovider.tsx
@@ -1,8 +1,6 @@
 "use client";
 import { ParallaxProvider } from "react-scroll-parallax";
 
-import { createContext } from "react";
-
 export default function ParallaxClientProvider({ children }: { children: React.ReactNode }) {
   return <ParallaxProvider>{children}</ParallaxProvider>;
 }
